c73887555bf34500e4f864ad6fd64710d37c0617
Nicholas Winters <nicholas.d.winters@gmail.com>
2021-04-06 16:44:51 -0500

    finished testing for followers in user model

--- test_user_model.py
+++ test_user_model.py
@@ -9,6 +9,7 @@ import os
 from unittest import TestCase
 
 from models import db, User, Message, Follows
+from sqlalchemy import exc
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -21,7 +22,7 @@ os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 # Now we can import app
 
 from app import app
-
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
@@ -35,24 +36,55 @@ class UserModelTestCase(TestCase):
     def setUp(self):
         """Create test client, add sample data."""
 
-        User.query.delete()
-        Message.query.delete()
-        Follows.query.delete()
+        db.drop_all()
+        db.create_all()
+        
+
+        user1 = User.signup("testuser", "test@test.com", "HASHED_PASSWORD", None)
+        user1_id = 1
+        user1.id = user1_id
+
+        user2 = User.signup("testuser2", "test2@test.com", "HASHED_PASSWORD2", None)
+        user2_id = 2
+        user2.id = user2_id
+
+        
+        db.session.commit()
+
+        u1 = User.query.get(user1_id)
+        u2 = User.query.get(user2_id)
+
+        self.u1 = u1
+        self.u2 = u2
 
         self.client = app.test_client()
 
+    def tearDown(self):
+        res = super().tearDown()
+        db.session.rollback()
+        return res
+       
+
     def test_user_model(self):
         """Does basic model work?"""
+       
+        # User should have no messages & no followers
+        self.assertEqual(len(self.u1.messages), 0)
+        self.assertEqual(len(self.u1.followers), 0)
 
-        u = User(
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD"
-        )
+    def test_user_repr(self):
+        '''does the repr display what is expected?'''
+        
+        self.assertEqual(repr(self.u1), f'<User #1: testuser, test@test.com>')
 
-        db.session.add(u)
+    def test_follow_functionality(self):
+        '''does the follow and unfollow functions work correctly?'''
+        self.u1.following.append(self.u2)
         db.session.commit()
 
-        # User should have no messages & no followers
-        self.assertEqual(len(u.messages), 0)
-        self.assertEqual(len(u.followers), 0)
+        self.assertEqual(len(self.u1.following), 1)
+        self.assertEqual(len(self.u2.followers), 1)
+        self.assertTrue(self.u1.is_following(self.u2))
+        self.assertFalse(self.u2.is_following(self.u1))
+        self.assertTrue(self.u2.is_followed_by(self.u1))
+        self.assertFalse(self.u1.is_followed_by(self.u2))
